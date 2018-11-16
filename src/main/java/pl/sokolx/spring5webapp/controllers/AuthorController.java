e12f29ec9d2721e8f4d5cb111898e156709d394d
SokolX <89soek@gmail.com>
2018-11-16 15:12:05 +0100

    fixed  authorController

--- src/main/java/pl/sokolx/spring5webapp/controllers/AuthorController.java
+++ src/main/java/pl/sokolx/spring5webapp/controllers/AuthorController.java
@@ -1,4 +1,24 @@
 package pl.sokolx.spring5webapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sokolx.spring5webapp.repositories.AuthorRespository;
+
+@Controller
 public class AuthorController {
+
+    private AuthorRespository authorRespository;
+
+    public AuthorController(AuthorRespository authorRespository) {
+        this.authorRespository = authorRespository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRespository.findAll());
+
+        return "authors";
+    }
 }
